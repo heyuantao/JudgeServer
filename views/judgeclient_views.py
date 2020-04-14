d3e99a83915f443f39ba168c5402b3c3bb8580c2
heyuantao <he_yuan_tao@163.com>
2020-04-14 14:16:16 +0800

    continue with api for judge client

--- views/judgeclient_views.py
+++ views/judgeclient_views.py
@@ -32,40 +32,38 @@ def api_test_judgeclient():
         logger.critical(traceback.format_exc())
         return jsonify({'status': 'error', 'message': 'Unknow error happend !'}), status.HTTP_400_BAD_REQUEST
 
-
-
-
-
+#the judge client share one api use different post params
 def api_problem_judge_common_view():
     try:
         if request.form.get('getpending', '') == '1':
-            _get_jobs_sub_view(request)
-        if request.form.get('update_solution', '') == '1':
-            _update_solution_sub_view(request)
-        if request.form.get('addceinfo', '') == '1':
-            _add_compile_error_information_sub_view(request)
-        if request.form.get('getsolution', '') == '1':
-            _get_solution_sub_view(request)
-        if request.form.get('getsolutioninfo', '') == '1':
-            _get_solution_information_sub_view(request)
-        if request.form.get('getprobleminfo', '') == '1':
-            _get_problem_information_sub_view(request)
-        if request.form.get('addreinfo', '') == '1':
-            _add_runing_error_information_sub_view(request)
-        if request.form.get('gettestdatalist', '') == '1':
-            _get_test_data_list_sub_view(request)
-        if request.form.get('gettestdata', '') == '1':
-            _get_test_data_sub_view(request)
-        if request.form.get('gettestdatadate', '') == '1':
-            _get_test_data_date_sub_view(request)
-        if request.form.get('updateproblem', '') == '1':
-            _update_problem_sub_view(request)
-        if request.form.get('updateuser', '') == '1':
-            _update_user_sub_view(request)
-        #if request.form.get('checklogin', '') == '1':
-        #     _check_login_sub_view(request)
-
-        #more work need tobe finished !
+            return _get_jobs_sub_view(request)
+        elif request.form.get('update_solution', '') == '1':
+            return _update_solution_sub_view(request)
+        elif request.form.get('addceinfo', '') == '1':
+            return _add_compile_error_information_sub_view(request)
+        elif request.form.get('getsolution', '') == '1':
+            return _get_solution_sub_view(request)
+        elif request.form.get('getsolutioninfo', '') == '1':
+            return _get_solution_information_sub_view(request)
+        elif request.form.get('getprobleminfo', '') == '1':
+            return _get_problem_information_sub_view(request)
+        elif request.form.get('addreinfo', '') == '1':
+            return _add_runing_error_information_sub_view(request)
+        elif request.form.get('gettestdatalist', '') == '1':
+            return _get_test_data_list_sub_view(request)
+        elif request.form.get('gettestdata', '') == '1':
+            return _get_test_data_sub_view(request)
+        elif request.form.get('gettestdatadate', '') == '1':
+            return _get_test_data_date_sub_view(request)
+        elif request.form.get('updateproblem', '') == '1':
+            return _update_problem_sub_view(request)
+        elif request.form.get('updateuser', '') == '1':
+            return _update_user_sub_view(request)
+        elif request.form.get('checklogin', '') == '1':
+            return _check_login_sub_view(request)
+        else:
+            logger.critical('Unsupport method in judgeclient_views.api_problem_judge_common_view() !')
+            return jsonify({'status': 'error', 'message': 'Unknow method !'}), status.HTTP_400_BAD_REQUEST
     except ValueError as e:
         logger.critical('Unknow method in judgeclient_views.api_problem_judge_common_view() !')
         logger.critical(traceback.format_exc())
@@ -107,11 +105,13 @@ def _get_test_data_date_sub_view(request):
     pass
 
 def _update_problem_sub_view(request):
-    pass
+    return jsonify({'status': 'success', 'message': 'nothing to do with update_problem !'}), status.HTTP_200_OK
 
 def _update_user_sub_view(request):
-    pass
+    return jsonify({'status': 'success', 'message': 'nothing to do with update_user !'}), status.HTTP_200_OK
 
+def _check_login_sub_view(request):
+    return jsonify({'status': 'success', 'message': 'Login check passed !'}), status.HTTP_200_OK
 
 
 
